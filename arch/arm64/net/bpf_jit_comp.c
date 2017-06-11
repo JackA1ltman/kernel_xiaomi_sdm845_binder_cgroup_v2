01fd65f9f94b3d718df7e867a96ded469d00a663
Daniel Borkmann <daniel@iogearbox.net>
2017-06-11 03:55:27 +0200

    bpf, arm64: take advantage of stack_depth tracking
    Make use of recently implemented stack_depth tracking for arm64 JIT,
    so that stack usage can be reduced heavily for programs not using
    tail calls at least.
    
    Change-Id: I7e1919f712a4c06827cefd01a8a55dd4bfd669fc
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -67,7 +67,7 @@ struct jit_ctx {
 	int idx;
 	int epilogue_offset;
 	int *offset;
-	__le32 *image;
+	u32 *image;
 	u32 stack_size;
 };
 
@@ -145,8 +145,7 @@ static inline int epilogue_offset(const struct jit_ctx *ctx)
 /* Stack must be multiples of 16B */
 #define STACK_ALIGN(sz) (((sz) + 15) & ~15)
 
-/* Tail call offset to jump into */
-#define PROLOGUE_OFFSET 7
+#define PROLOGUE_OFFSET 8
 
 static int build_prologue(struct jit_ctx *ctx)
 {
@@ -197,6 +196,13 @@ static int build_prologue(struct jit_ctx *ctx)
 
 	/* Initialize tail_call_cnt */
 	emit(A64_MOVZ(1, tcc, 0, 0), ctx);
+
+	/* 4 byte extra for skb_copy_bits buffer */
+	ctx->stack_size = prog->aux->stack_depth + 4;
+	ctx->stack_size = STACK_ALIGN(ctx->stack_size);
+
+	/* Set up function call stack */
+	emit(A64_SUB_I(1, A64_SP, A64_SP, ctx->stack_size), ctx);
 
 	cur_offset = ctx->idx - idx0;
 	if (cur_offset != PROLOGUE_OFFSET) {
